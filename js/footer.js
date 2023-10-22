526309914ae2467cd2895ecac20bd133617a79fa
Christian Lundt <cbl@dr.com>
2023-10-22 18:42:49 +0200

    Footer JS update

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+// Load the footer content using JavaScript
+        fetch('../common/footer.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('footer-placeholder').innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Error loading footer: ' + error);
+            });
